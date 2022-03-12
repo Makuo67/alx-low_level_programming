066b8f150a7bbf1790e673d33e234204f1448ab1
Makuo67 <okekemakuo67@gmail.com>
2022-03-11 19:11:02 -0800

    Adding a program that prints single digit numbers from base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all single digit numbers of basr 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+printf("%d", n);
+printf("\n");
+return (0);
+}
