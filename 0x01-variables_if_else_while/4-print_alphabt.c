47a146e8b5621b4a29d44b827e7cc93639376510
Makuo67 <okekemakuo67@gmail.com>
2022-03-11 18:55:25 -0800

    Adding a program that print alphabet in lower case

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints the alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char ch = 'a';
+  
+  while (ch <= 'z')
+    {
+      if (ch != 'e' && ch != 'q')
+	{
+	  putchar(ch);
+	}
+      ch++;
+    }
+  putchar('\n');
+  return (0);
+}
